9e00423672d6b31e7d20cdf77f0eb7d759451f90
Myriam MBANZULU <myriam.mbanzulu@tessi.fr>
2025-11-20 09:48:09 +0100

    Maj pour la page alertes

--- src/pages/AlertsPage.tsx
+++ src/pages/AlertsPage.tsx
@@ -1,5 +1,11 @@
+import { Footer } from "../components/layout/Footer";
+import { HeaderService } from "../components/layout/HeaderService";
+
 export default function AlertsPage() {
     return(
-        <div></div>
+        <div className="min-h-screen bg-slate-950 text-slate-100">
+            <HeaderService />
+            <Footer />
+        </div>
     );
 }
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import LoginPage from "../pages/auth/LoginPage";
 import RegisterPage from "../pages/auth/RegisterPage";
 import LandingPage from "../pages/LandingPage";
+import AlertsPage from "../pages/AlertsPage";
 
 export function AppRouter() {
   return (
@@ -10,6 +11,7 @@ export function AppRouter() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/alerts" element={<AlertsPage />} />
       </Routes>
     </BrowserRouter>
   );
